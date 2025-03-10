1ed67ef430429fb2000f997d81bf36195761c79a
DEBJYOTI ADAK <133900810+Deb2oo@users.noreply.github.com>
2025-03-10 19:32:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 let display = document.getElementById('display');
 
 function appendValue(value) {
-    if (value === 'Math.sqrt(' || value === 'Math.sin(' || value === 'Math.cos(' || 
-        value === 'Math.tan(' || value === 'Math.log10(' || value === 'Math.exp(' || 
-        value === 'Math.pow(') {
-        display.value += value + '(';
-    } else {
-        display.value += value;
-    }
+    display.value += value;
 }
 
 function clearDisplay() {
@@ -22,11 +16,11 @@ function calculateResult() {
     try {
         let expression = display.value
             .replace(/√/g, "Math.sqrt")
-            .replace(/sin/g, "Math.sin")
-            .replace(/cos/g, "Math.cos")
-            .replace(/tan/g, "Math.tan")
-            .replace(/log/g, "Math.log10")
-            .replace(/exp/g, "Math.exp")
+            .replace(/sin\(/g, "Math.sin(")
+            .replace(/cos\(/g, "Math.cos(")
+            .replace(/tan\(/g, "Math.tan(")
+            .replace(/log\(/g, "Math.log10(")
+            .replace(/exp\(/g, "Math.exp(")
             .replace(/x\^y/g, "**");
 
         display.value = eval(expression);
@@ -34,4 +28,3 @@ function calculateResult() {
         display.value = 'Error';
     }
 }
-
