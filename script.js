7c4213b878f566e6caf3a565e7c6690e7b57b93c
DEBJYOTI ADAK <133900810+Deb2oo@users.noreply.github.com>
2025-03-10 19:36:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,13 @@ function deleteLast() {
 function calculateResult() {
     try {
         let expression = display.value
-            .replace(/√/g, "Math.sqrt")
-            .replace(/sin\(/g, "Math.sin(")
-            .replace(/cos\(/g, "Math.cos(")
-            .replace(/tan\(/g, "Math.tan(")
-            .replace(/log\(/g, "Math.log10(")
-            .replace(/exp\(/g, "Math.exp(")
+            .replace(/Math.sqrt\(/g, "Math.sqrt")
+            .replace(/Math.pow\(/g, "Math.pow")
+            .replace(/Math.sin\(/g, "Math.sin")
+            .replace(/Math.cos\(/g, "Math.cos")
+            .replace(/Math.tan\(/g, "Math.tan")
+            .replace(/Math.log10\(/g, "Math.log10")
+            .replace(/Math.exp\(/g, "Math.exp")
             .replace(/x\^y/g, "**");
 
         display.value = eval(expression);
