17004cc00ae86c0ee0035602232a7e5db1a0a4ef
DEBJYOTI ADAK <133900810+Deb2oo@users.noreply.github.com>
2025-03-10 19:39:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,17 @@ function calculateResult() {
     try {
         let expression = display.value
             .replace(/Math.sqrt\(/g, "Math.sqrt")
-            .replace(/Math.pow\(/g, "Math.pow")
+            .replace(/Math.pow\(/g, "Math.pow") 
             .replace(/Math.sin\(/g, "Math.sin")
             .replace(/Math.cos\(/g, "Math.cos")
             .replace(/Math.tan\(/g, "Math.tan")
             .replace(/Math.log10\(/g, "Math.log10")
             .replace(/Math.exp\(/g, "Math.exp")
-            .replace(/x\^y/g, "**");
+            .replace(/x\^y/g, "**"); 
+
+        expression = expression.replace(/Math\.sin\(([^)]+)\)/g, (match, p1) => `Math.sin(${p1} * (Math.PI / 180))`);
+        expression = expression.replace(/Math\.cos\(([^)]+)\)/g, (match, p1) => `Math.cos(${p1} * (Math.PI / 180))`);
+        expression = expression.replace(/Math\.tan\(([^)]+)\)/g, (match, p1) => `Math.tan(${p1} * (Math.PI / 180))`);
 
         display.value = eval(expression);
     } catch (error) {
